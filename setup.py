ad330b788908cda88147064acc6b85ab4bf55d13
Matheus Fillipe <matheusfillipeag@gmail.com>
2022-12-12 11:21:27 -0300

    Test deploy

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="extra_streamlit_components",
-    version="0.1.55",
+    name="extra_streamlit_components_better_cookie_manager",
+    version="0.0.1",
     author="Mohamed Abdou",
     author_email="matex512@gmail.com",
     description="An all-in-one place, to find complex or just natively unavailable components on streamlit.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Mohamed-512/Extra-Streamlit-Components",
+    url="https://github.com/matheusfillipe/Extra-Streamlit-Components",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[
